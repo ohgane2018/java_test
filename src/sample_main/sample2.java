c6c8dde3a296af815414ef11024ba362eb9dbd7d
ohgan <ohgan@LAPTOP-RREOMDA8>
2020-10-30 10:05:10 +0900

    AT

--- src/sample_main/sample2.java
+++ src/sample_main/sample2.java
@@ -28,7 +28,7 @@ public class sample2 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served AT: ").append(request.getContextPath());
 	}
 
 	/**
